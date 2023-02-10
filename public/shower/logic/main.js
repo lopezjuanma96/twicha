6b0eba0dbc627786e042d62fc61521b7156824e7
Juanma López <lopezjuanma96@gmail.com>
2023-02-10 07:38:39 -0300

    improved structure of showerHTML

--- public/shower/logic/main.js
+++ public/shower/logic/main.js
@@ -1,17 +1,33 @@
 const socket = io();
 
+const newPostTitle = document.getElementById("newPostTitle");
+const newPostText = document.getElementById("newPostText");
 const postList = document.getElementById("postList");
+
+let lastPost;
 
 socket.on("ip", (value) => {
     console.log(value)
 })
 
 socket.on("distribute", post => {
-    if (post) {
-        console.log(post.user, 'sent', post.message);
+    if (lastPost) {
         const postElement = document.createElement('div')
         postElement.className = "postElement"
-        postElement.innerHTML = `<b class="postUser">${post.user}: </b><span class="postMsg">${post.message}</span>`
+        postElement.innerHTML = `<b class="postUser">${lastPost.user}: </b><span class="postMsg">${lastPost.message}</span>`
         postList.append(postElement)
-    } else console.log('no new messages')
+        lastPost = undefined;
+    }
+    if (post) {
+        console.log(post.user, 'sent', post.message);
+        if (post.permanent){;}
+        else{
+            newPostTitle.innerHTML = `${post.user} just said..`;
+            newPostText.innerHTML = post.message;
+            lastPost = post;
+        }
+    } else {
+        newPostTitle.innerHTML = "Nobody has said anything yet.."
+        newPostText.innerHTML = "Be the first one! Scan the QR code below!"
+    }
 })
